60fb0f03fc5fe5ef333b63087de3440b1582f786
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-21 20:20:53 +0600

    nav

---
+++ app/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="navbar">
+        <ul class="flex space-x-4">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/events">Events</NuxtLink></li>
+            <li><NuxtLink to="/events/1">Event 1</NuxtLink></li>
+            <li><NuxtLink to="/events/2">Event 2</NuxtLink></li>
+        </ul>
+    </nav>
+</template>
