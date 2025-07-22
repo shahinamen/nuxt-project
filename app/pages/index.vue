6093521fae8ef712b25630731531b2ca549ad0a2
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-22 08:22:40 +0600

    assets and icon added

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,12 @@
   <div>
     <h1 class="text-4xl">This is Nuxt Project</h1>
     <Alert title="My Alert" />
+    <IconsBell class="text-8xl text-indigo-600" />
     <ProfileHeader />
+    <div>
+      <img src="@/assets/images/1.png" alt="Discover Nuxt" />
+      <img src="@/assets/images/2.png" alt="Discover Nuxt" />
+      <img src="/1.png" alt="Discover Nuxt" />
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
     "@": resolve(__dirname, "./"),
+    "assets" : '/<rootDir>/assets',
+    "components": resolve(__dirname, "./components"),
+    "composables": resolve(__dirname, "./composables"),
+    "layouts": resolve(__dirname, "./layouts"),
+    "pages": resolve(__dirname, "./pages"),
+    "public": resolve(__dirname, "./public"),
+    "plugins": resolve(__dirname, "./plugins"),
+    "server": resolve(__dirname, "./server"),
+    "utils": resolve(__dirname, "./utils"),
   },
   css: [
     '@/assets/main.css',
