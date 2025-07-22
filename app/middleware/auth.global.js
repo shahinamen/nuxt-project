70da2d512063f6c90d59c85d7b4a354c107d6da6
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-22 23:36:26 +0600

    middleware added

---
+++ app/middleware/auth.global.js
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+   // const isLoggedIn = true; // Replace with actual authentication check logic
+    //if (isLoggedIn) {
+        //return navigateTo('custom'); // Redirect to login page if not authenticated
+    //}
+   // return navigateTo('/')
+  // Middleware logic to check authentication
+  //const isAuthenticated = useAuth().isAuthenticated();
+
+  //if (!isAuthenticated) {
+    // Redirect to login page if not authenticated
+  //  return navigateTo('/login');
+  //}
+
+  // Optionally, you can add more logic here, like checking user roles or permissions
+});
--- app/plugins/myPlugin.js
+++ app/plugins/myPlugin.js
@@ -1,6 +1,5 @@
 export default defineNuxtPlugin((nuxtApp) => {  
     nuxtApp.provide('useUtils', useUtils());
-    console.log(nuxtApp);
     return {
         provide: {  
             hello: 'Hello from myPlugin!'
