51a749560ee119208479dcee67c8bb677fe3551c
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-23 22:42:55 +0600

    server api and routes added

--- app/pages/custom.vue
+++ app/pages/custom.vue
@@ -6,6 +6,7 @@ definePageMeta({
 import { useCounterStore } from '~/stores/myStore';
 const store = useCounterStore();
 
+const { data, pending, error } = await useFetch('/api/hello')
 
 import { useMouse } from '@vueuse/core'
 
@@ -21,9 +22,18 @@ const nuxtApp = useNuxtApp();
   <h1>hello {{ nuxtApp.$hello }}</h1>
   <div>
     <h1>Counter</h1>
-    <h2>{{ store.count }}</h2>
+    <h2 class="text-2xl">{{ store.count }}</h2>
     <button class="cursor-pointer m-2 border-1 border-green-500" @click="store.increment">Increment</button>
     <button class="cursor-pointer m-2 border-1 border-green-500" @click="store.decrement">Decrement</button>
     <button class="cursor-pointer m-2 border-1 border-green-500" @click="store.reset">Reset</button>
+  </div>
+  <div>
+    <h1 class="text-4xl">
+      <div>
+    <p v-if="pending">Loading...</p>
+    <p v-else-if="error">Error: {{ error.message }}</p>
+    <p v-else>Server API Response: {{ data }}</p>
+  </div>
+    </h1>
   </div>
 </template>
